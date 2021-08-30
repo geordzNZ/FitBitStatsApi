9e1b9b46823d04f375bee0c492a89f610ea6352d
Geordie King <geordie.king@gmail.com>
2021-08-30 19:18:14 +0100

    update Index and JS files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkM4Vk4iLCJzdWIiOiIyQ0pHSEQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNjMyOTM5MjE1LCJpYXQiOjE2MzAzNDcyMTV9.lGH9vBzjXt2luv2ThLY5b7PabBCBEA7AzLrxEQ-SRhQ"
+
+
+fetch('https://api.fitbit.com/1/user/-/profile.json',{
+  method: "GET",
+  headers: {"Authorization": "Bearer " + access_token}
+})
+.then(response => response.json())
+.then(json => console.log(json))
