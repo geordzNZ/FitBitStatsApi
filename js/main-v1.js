d6d1178a78ccd94a334099951f7c95f1850a2ae6
Geordie King <geordie.king@gmail.com>
2021-09-03 01:59:07 +0100

    try to extract to sssh

--- js/main-v1.js
+++ js/main-v1.js
@@ -1,4 +1,3 @@
-
 const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkM4Vk4iLCJzdWIiOiIyQ0pHSEQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNjMyOTM5MjE1LCJpYXQiOjE2MzAzNDg2NzJ9.f1iyj9s0pAfA_SoQHWCLnz4Rp-QZWOsBbpUfM94UMcs"
 
 
@@ -22,6 +21,10 @@ document.querySelector('#btnPastSteps2').addEventListener('click', getPastSteps2
 
 
 function getProfile(){
+  import { myName } from './shhh';
+  let val = myName;
+  console.log(val);
+
   fetch('https://api.fitbit.com/1/user/-/profile.json',{
     method: "GET",
     headers: {"Authorization": "Bearer " + access_token}
@@ -32,6 +35,7 @@ function getProfile(){
 
 
 function getTodaySteps(){
+
   let res = ''
   fetch('https://api.fitbit.com/1/user/-/activities/steps/date/today/1d.json',{
     method: "GET",
